86d09a83a8152a8cf582b304f856d39f6880f676
Nikita <nikitkavershinin2019@gmail.com>
2022-12-30 11:05:21 +0300

    new form

--- IS/Forms/NextForm.cs
+++ IS/Forms/NextForm.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace IS.Forms
 {
+    enum RowState
+    {
+        Existed,
+        New,
+        Modified,
+        ModifiedNew,
+        Deleted
+    }
     public partial class NextForm : Form
     {
+        DataBase dataBase = new DataBase();
+
+        int selecedRow;
+        
         public NextForm()
         {
             InitializeComponent();
+        }
+
+        private void CreateColums()
+        {
+            dataGridView1.Columns.Add("id", "id");
+            dataGridView1.Columns.Add("type_of", "Тип материала");
+            dataGridView1.Columns.Add("count_of", "Количество");
+            dataGridView1.Columns.Add("title", "Название");
+            dataGridView1.Columns.Add("content", "Текст");
+            dataGridView1.Columns.Add("price", "Цена");
+            dataGridView1.Columns.Add("IsNew", String.Empty);
+            
+        }
+
+        private void ReadSingleRow(DataGridView dgw, IDataRecord record)
+        {
+            dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetInt32(2), record.GetString(3), record.GetString(4), record.GetInt32(5), RowState.ModifiedNew);
+        }
+
+        private void RefrestDataGrid(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+
+            string queryString = $"select * from posts";
+
+            SqlCommand command = new SqlCommand(queryString, dataBase.getConnection());
+
+            dataBase.openConnection();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                ReadSingleRow(dgw, reader);
+            }
+            reader.Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NextForm_Load(object sender, EventArgs e)
+        {
+            CreateColums();
+            RefrestDataGrid(dataGridView1);
         }
     }
 }
